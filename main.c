801cfeaa503e90b3af28414f7f254080778d6d8b
Filip <filipporeba967@gmail.com>
2022-09-06 16:43:37 +0200

    sizeof operator

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int array[] = {3, 6, 8, 5, 9};
-    for (int i = 0; i < 5; i++) {
+    int array[] = {3, 6, 8, 5, 9, 4, 2};
+    int size = sizeof array / 4;
+    printf("size=%i\n", size);
+    for (int i = 0; i < size; i++) {
         printf("%i\n", array[i]);
     }
     return 0;
