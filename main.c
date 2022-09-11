f60ef495e0ce0733c0266b50675561e09648a3da
Filip <filipporeba967@gmail.com>
2022-09-11 10:16:32 +0200

    average function

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    int array[] = {3, 6, 8, 5, 9, 4, 2};
-    int size = sizeof array / 4;
-    printf("size=%i\n", size);
+double average(double data[], int size) {
+    double sum = 0;
     for (int i = 0; i < size; i++) {
-        printf("%i\n", array[i]);
+        sum = sum + data[i];
     }
+    return sum / size;
+}
+
+int main() {
+    double array[] = {3, 6, 8, 5, 9, 4, 2};
+    int size = sizeof array / 8;
+    double avg = average(array, size);
+    printf("average = %f", avg);
     return 0;
 }
